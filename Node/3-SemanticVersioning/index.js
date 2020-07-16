18a6e9ba754bf89e1fe39e2c6e014a11003e3a20
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-16 17:41:40 -0400

    added notes

---
+++ Node/3-SemanticVersioning/index.js
@@ -0,0 +1,6 @@
+// Semantic Versioning aka SemVer
+// Version 4.13.6 => Major.Minor.Patch (Major version, Minor version, Patch version)
+
+// Bug fixes => Patch + 1
+// Feature additions without breaking the existing API => Minor.version + 1 
+// Feature additions that break the existing API => Major.version + 1
