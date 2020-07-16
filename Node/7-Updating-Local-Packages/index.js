9f57bc01aee0931794af34eb35ed9cf7c6840e2d
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-16 17:56:42 -0400

    added notes

---
+++ Node/7-Updating-Local-Packages/index.js
@@ -0,0 +1,4 @@
+// Get outdated dependency data => npm outdated
+// To update package => npm update, updates
+// Get latest package updates => npm i -g npm-check-updates
+// Then run npm-check-updates or ncu -u
