5a08a1a8ebb9b5a37f67f3c5d2648665f951915b
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-17 09:14:09 -0400

    added notes

---
+++ Node/Section-3-Express-Advanced-Topics/1-Creating-Custom-Middleware/index.js
@@ -0,0 +1,103 @@
+import joi from 'joi'
+import log from './logger.js'
+import authenticate from './auth.js'
+import express from 'express'
+const app = express();
+const Joi = joi
+
+// Adding a piece of middleware for 'app' to use
+app.use(express.json())
+
+// Custom middleware
+app.use(log)
+app.use(authenticate)
+
+const courses = [{
+          id: 1,
+          name: "course1",
+     },
+     {
+          id: 2,
+          name: "course2",
+     },
+     {
+          id: 3,
+          name: "course3",
+     },
+];
+
+// ===================================
+
+// GET Request - Root (READ)
+app.get('/', (req, res) => {
+     res.send("Hello Willis")
+})
+
+// GET Request (READ)
+app.get('/api/courses', (req, res) => {
+     res.send(courses)
+})
+
+// Includes request parameters (READ/SHOW)
+app.get('/api/courses/:id', (req, res) => {
+     const foundCourse = courses.find(course => course.id === parseInt(req.params.id))
+     if (!foundCourse) return res.status(404).send(`The course with ID ${req.params.id} was not found.`)
+     res.send(foundCourse)
+})
+
+// POST Request (CREATE)
+app.post('/api/courses', (req, res) => {
+     const {
+          error
+     } = validateCourse(req.body);
+     if (error) return res.status(400).send(error.details[0].message);
+
+     if (result.error) return res.status(400).send(result.error.details[0].message)
+
+     const course = {
+          id: courses.length + 1,
+          name: req.body.name
+     }
+     courses.push(course)
+     res.send(courses)
+
+})
+
+// PUT Request (UPDATE)
+app.put('/api/courses/:id', (req, res) => {
+     // Look up the course 
+     const foundCourse = courses.find(course => course.id === parseInt(req.params.id))
+     if (!foundCourse) return res.status(404).send("We couldn't find that course")
+
+     const {
+          error
+     } = validateCourse(req.body)
+     if (error) return res.status(400).send(error.details[0].message);
+
+     foundCourse.name = req.body.name
+     res.send(foundCourse)
+
+})
+
+// DELETE Request (DELETE)
+app.delete('/api/courses/:id', (req, res) => {
+     const foundCourse = courses.find((course) => course.id === parseInt(req.params.id));
+     if (!foundCourse) return res.status(404).send("We couldn't find that course");
+
+     const index = courses.indexOf(foundCourse)
+     courses.splice(index, 1)
+     res.send(foundCourse)
+})
+
+// ===================================
+
+const validateCourse = course => {
+     const schema = Joi.object({
+          name: Joi.string().min(3).required(),
+     });
+     return schema.validate(course);
+}
+
+// =============PORT==============
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`))
