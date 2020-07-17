3550d50d6d9d7ac2db17756ef07b61987c11d324
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-17 18:56:10 -0400

    added notes

---
+++ 0-Node/Section-3-Express-Advanced-Topics/7-Templating-Engines/routes/courses.js
@@ -0,0 +1,83 @@
+import express from 'express'
+const coursesRouter = express.Router();
+
+const courses = [{
+          id: 1,
+          name: "course1",
+     },
+     {
+          id: 2,
+          name: "course2",
+     },
+     {
+          id: 3,
+          name: "course3",
+     },
+];
+
+// GET Request (READ)
+coursesRouter.get('/', (req, res) => {
+     res.send(courses)
+})
+
+// Includes request parameters (READ/SHOW)
+coursesRouter.get('/:id', (req, res) => {
+     const foundCourse = courses.find(course => course.id === parseInt(req.params.id))
+     if (!foundCourse) return res.status(404).send(`The course with ID ${req.params.id} was not found.`)
+     res.send(foundCourse)
+})
+
+// POST Request (CREATE)
+coursesRouter.post('/', (req, res) => {
+     const {
+          error
+     } = validateCourse(req.body);
+     if (error) return res.status(400).send(error.details[0].message);
+
+     if (error) return res.status(400).send(error.details[0].message)
+
+     const course = {
+          id: courses.length + 1,
+          name: req.body.name
+     }
+     courses.push(course)
+     res.send(courses)
+
+})
+
+// PUT Request (UPDATE)
+coursesRouter.put('/:id', (req, res) => {
+     // Look up the course 
+     const foundCourse = courses.find(course => course.id === parseInt(req.params.id))
+     if (!foundCourse) return res.status(404).send("We couldn't find that course")
+
+     const {
+          error
+     } = validateCourse(req.body)
+     if (error) return res.status(400).send(error.details[0].message);
+
+     foundCourse.name = req.body.name
+     res.send(foundCourse)
+
+})
+
+// DELETE Request (DELETE)
+coursesRouter.delete('/:id', (req, res) => {
+     const foundCourse = courses.find((course) => course.id === parseInt(req.params.id));
+     if (!foundCourse) return res.status(404).send("We couldn't find that course");
+
+     const index = courses.indexOf(foundCourse)
+     courses.splice(index, 1)
+     res.send(foundCourse)
+})
+
+// ===================================
+
+const validateCourse = course => {
+     const schema = Joi.object({
+          name: Joi.string().min(3).required(),
+     });
+     return schema.validate(course);
+}
+
+export default coursesRouter
