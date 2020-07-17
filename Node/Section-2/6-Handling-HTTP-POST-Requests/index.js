51bdcbe2815d63696e22851ea6b1948e111e24c0
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-16 21:03:27 -0400

    added notes

---
+++ Node/Section-2/6-Handling-HTTP-POST-Requests/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+const app = express();
+
+const courses = [{
+          id: 1,
+          name: "course1",
+     },
+     {
+          id: 2,
+          name: "course2",
+     },
+     {
+          id: 3,
+          name: "course3",
+     },
+];
+
+app.get('/', (req, res) => {
+     res.send("Hello Willis")
+})
+
+app.get('/api/courses', (req, res) => {
+     res.send(courses)
+})
+
+// Includes request parameters 
+app.get('/api/courses/:id', (req, res) => {
+     const foundCourse = courses.find(course => course.id === parseInt(req.params.id))
+     if (!foundCourse) res.status(404).send(`The course with ID ${req.params.id} was not found.`)
+     else res.send(foundCourse)
+})
+
+// =============PORT==============
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`))
