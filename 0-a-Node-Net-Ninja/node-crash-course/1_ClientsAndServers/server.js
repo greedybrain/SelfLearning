98387caacb8a9e25569670aa4f5c1f0de52712e4
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-10-15 12:38:30 -0400

    ...

--- 0-a-Node-Net-Ninja/node-crash-course/1_ClientsAndServers/server.js
+++ 0-a-Node-Net-Ninja/node-crash-course/1_ClientsAndServers/server.js
@@ -16,12 +16,20 @@ const server = http.createServer((req, res) => {
         switch(req.url) {
                 case '/':
                         path += '/index.html'
+                        res.statusCode = 200
                         break;
                 case '/about':
                         path += '/about.html'
+                        res.statusCode = 200
+                        break;
+                case '/about-me':
+                        res.statusCode = 301
+                        res.setHeader('Location', '/about')
+                        res.end()
                         break;
                 default:
                         path += '/404.html'
+                        res.statusCode = 404
                         break;
         }
 
