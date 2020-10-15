42d4fde6ffed87ef591d43cf0660392605d5f278
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-10-15 16:01:20 -0400

    ...

--- 0-a-Node-Net-Ninja/node-crash-course/1_ClientsAndServers/app.js
+++ 0-a-Node-Net-Ninja/node-crash-course/1_ClientsAndServers/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 // setup a express app 
 const app = express();
 
+// register view engine 
+app.set('view engine', 'ejs');
+
 // listen for requests 
 app.listen(3000)
 
